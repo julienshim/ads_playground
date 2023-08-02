2d8ffdbe79a8a4d4bed435c988e8502fbc7c18be
julienshim <25723428+julienshim@users.noreply.github.com>
2023-08-02 01:33:55 -0700

    cf

---
+++ recursion/capitalizeFirst.js
@@ -0,0 +1,16 @@
+function capitalizeFirst(arr) {
+  // add whatever parameters you deem necessary - good luck!
+  let newArr = [];
+
+  if (arr.length === 0) {
+    return newArr;
+  }
+
+  newArr.push(arr[0][0].toUpperCase() + arr[0].slice(1));
+
+  newArr = newArr.concat(capitalizeFirst(arr.slice(1)));
+
+  return newArr;
+}
+
+console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
