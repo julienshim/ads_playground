8b3aaaba6af281da5bf55d21f6f5783c5b8d789e
julienshim <25723428+julienshim@users.noreply.github.com>
2023-08-03 21:22:06 -0700

    cs

--- recursion/collectStrings.js
+++ recursion/collectStrings.js
@@ -1,18 +1,3 @@
-// let newObj = {};
-
-// for (let item in obj) {
-//   let value = obj[item];
-//   if (typeof value === "number") {
-//     newObj[item] = String(value);
-//   } else if (typeof value === "object" && !Array.isArray(value)) {
-//     newObj[item] = stringifyNumbers(value);
-//   } else {
-//     newObj[item] = value;
-//   }
-// }
-
-// return newObj;
-
 function collectStrings(obj) {
   let arr = [];
 
