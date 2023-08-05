fd1076ae9aff36c77955b33ccf1317e23e93d0b8
julienshim <25723428+julienshim@users.noreply.github.com>
2023-08-05 01:35:25 -0700

    ip

---
+++ recursion/isPalindrome.js
@@ -0,0 +1,20 @@
+function isPalindrome(str) {
+  // add whatever parameters you deem necessary - good luck!
+  if (str.length === 1) {
+    return true;
+  }
+
+  if (str[0] !== str[str.length - 1]) {
+    return false;
+  }
+
+  return isPalindrome(str.slice(1, str.length - 1));
+}
+
+console.log(isPalindrome("awesome")); // false
+console.log(isPalindrome("foobar")); // false
+console.log(isPalindrome("tacocat")); // true
+console.log(isPalindrome("amanaplanacanalpanama")); // true
+console.log(isPalindrome("amanaplanacanalpandemonium")); // false
+
+// if(str.length === 2) return str[0] === str[1]; // is this necessary?
