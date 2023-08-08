fc1a43b35d479b54d6ff3a8d4af9de6c697a98ee
julienshim <25723428+julienshim@users.noreply.github.com>
2023-08-08 01:36:29 -0700

    sr

---
+++ recursion/someRecursive.js
@@ -0,0 +1,18 @@
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+function someRecursive(arr, cb) {
+  // add whatever parameters you deem necessary - good luck!
+  if (arr.length === 0) return false;
+
+  if (cb(arr[0])) {
+    return true;
+  }
+
+  return someRecursive(arr.slice(1), cb);
+}
+
+console.log(someRecursive([1, 2, 3, 4], isOdd)); // true
+console.log(someRecursive([4, 6, 8, 9], isOdd)); // true
+console.log(someRecursive([4, 6, 8], isOdd)); // false
+console.log(someRecursive([4, 6, 8], (val) => val > 10)); // false
